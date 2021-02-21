7e6f80028c0f852cf34d267ceb80edebc2bcb91b
Warley Máximo de Matos Pereira <warleym.matos@gmail.com>
2021-02-21 00:21:31 -0300

    Inserido as variáveis

--- app/src/main/java/br/com/progiv/simplecalculadora/MainActivity.java
+++ app/src/main/java/br/com/progiv/simplecalculadora/MainActivity.java
@@ -3,8 +3,21 @@ package br.com.progiv.simplecalculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int i = 0; //índice de números para operação
+    int count = 0; //Qte de caracteres de números. Máximo 7
+    int[] Result; //Array nos números digitados
+    int Total = 0; //Resultado
+    static int INVALID = 9999999; //variável estática para controle de qte de dígitos
+    String operacao; //Operação a ser executada
+
+    TextView RESULTSCREEN;
+    Button btn01, btn02, btn03, btn04, btn05, btn06, btn07, btn08, btn09, btn00;
+    Button btnSoma, btnSub, btnMult, btnDiv, btnIgual, btnLimp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
